36b50804d16dc39be95ccb44564b1e2b6962a164
tehpsalmist <bstewardcodes@gmail.com>
2021-05-04 04:40:19 -0400

    crypt of the damned

--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
 import React from 'react'
 import { render } from 'react-dom'
+import BeastTyrant from './assets/1.svg'
+import Berserker from './assets/2.svg'
+import Brute from './assets/3.svg'
+import Doomstalker from './assets/4.svg'
+import Elementalist from './assets/5.svg'
+import MindThief from './assets/6.svg'
+import NightShroud from './assets/7.svg'
+import Plagueherald from './assets/8.svg'
+import Spellweaver from './assets/9.svg'
+import Summoner from './assets/10.svg'
+import Sunkeeper from './assets/11.svg'
+import Tinkerer from './assets/12.svg'
+import Soothsinger from './assets/13.svg'
+import Scoundrel from './assets/14.svg'
+import Sawbones from './assets/15.svg'
+import Quartermaster from './assets/16.svg'
+import Cragheart from './assets/17.svg'
+import Wind from './assets/wind.svg'
+import Fire from './assets/fire.svg'
+import Sun from './assets/sun.svg'
+import Frost from './assets/frost.svg'
+import Leaf from './assets/leaf.svg'
+import Moon from './assets/moon.svg'
 
 export const App = props => {
-  return <main>Hello World!</main>
+  return <div>
+    <header className='print:hidden flex-center flex-col space-y-2 py-4'>
+      <h1 className='text-2xl'>Gloomhaven Printables</h1>
+      <p className='italic'>
+        <strong>Instructions:</strong>{' '}
+        Print on plain, letter-sized paper with a laser printer.
+      </p>
+      <button type='button' onClick={e => print()} className='p-2 rounded shadow-md bg-purple-400 hover:bg-purple-500 focus:bg-purple-600 text-white'>Print</button>
+    </header>
+    <main className='w-8.5in h-11in border print:border-0 m-auto p-0.5in'>
+      <section className='h-full w-full grid grid-cols-5 row-auto col-auto place-items-center border print:border-0'>
+        <BeastTyrant height='1in' width='1in' />
+        <Berserker height='1in' width='1in' />
+        <Brute height='1in' width='1in' />
+        <Cragheart height='1in' width='1in' />
+        <Doomstalker height='1in' width='1in' />
+        <Elementalist height='1in' width='1in' />
+        <MindThief height='1in' width='1in' />
+        <NightShroud height='1in' width='1in' />
+        <Plagueherald height='1in' width='1in' />
+        <Spellweaver height='1in' width='1in' />
+        <Summoner height='1in' width='1in' />
+        <Sunkeeper height='1in' width='1in' />
+        <Tinkerer height='1in' width='1in' />
+        <Soothsinger height='1in' width='1in' />
+        <Scoundrel height='1in' width='1in' />
+        <Sawbones height='1in' width='1in' />
+        <Quartermaster height='1in' width='1in' />
+        <Wind height='1in' width='1in' />
+        <Sun height='1in' width='1in' />
+        <Fire height='1in' width='1in' />
+        <Frost height='1in' width='1in' />
+        <Leaf height='1in' width='1in' />
+        <Moon height='1in' width='1in' />
+        <Leaf className='text-black' height='1in' width='1in' />
+      </section>
+    </main>
+  </div>
 }
 
 render(<App />, document.getElementById('app'))
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        leaf: '#8aa63d',
+        fire: '#e25426',
+        sun: '#efac1e',
+        frost: '#15c2ee',
+        moon: '#1e2c35',
+        wind: '#9ab0b7',
         primary: colors.blue,
         secondary: {
           ...colors.green,
@@ -35,6 +41,30 @@ module.exports = {
         ...theme('spacing')
       }),
       spacing: {
+        '0.5in': '0.5in',
+        '1in': '1in',
+        '1.5in': '1.5in',
+        '2in': '2in',
+        '2.5in': '2.5in',
+        '3in': '3in',
+        '3.5in': '3.5in',
+        '4in': '4in',
+        '4.5in': '4.5in',
+        '5in': '5in',
+        '5.5in': '5.5in',
+        '6in': '6in',
+        '6.5in': '6.5in',
+        '7in': '7in',
+        '7.5in': '7.5in',
+        '8in': '8in',
+        '8.5in': '8.5in',
+        '9in': '9in',
+        '9.5in': '9.5in',
+        '10in': '10in',
+        '10.5in': '10.5in',
+        '11in': '11in',
+        '11.5in': '11.5in',
+        '12in': '12in',
         36: '9rem',
         xs: '20rem',
         sm: '24rem',
@@ -51,6 +81,9 @@ module.exports = {
       },
       boxShadow: {
         md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06), 0 -1px 4px -1px rgba(0, 0, 0, 0.06)'
+      },
+      screens: {
+        print: { raw: 'print' }
       }
     },
   },
